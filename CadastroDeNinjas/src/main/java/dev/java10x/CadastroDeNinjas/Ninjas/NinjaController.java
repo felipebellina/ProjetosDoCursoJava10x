b37960082047a1134f44345bc8d476eebcb17627
Felipe Bellina <felipeluizb@hotmail.com>
2025-10-12 13:21:17 -0300

    chore: .gitignore adicionado as minhas credencias do db

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class NinjaController {
 
-    @GetMapping("/boas-vindas")
+    @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota";
     }
